7912277b57c57e5763d5c6f92c366c7f52422747
Кодиров Султон <sultonkodiro109@gmail.com>
2020-05-22 16:55:17 +0500

    Основная функцианальност добавлено

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        PersonRepository person = new PersonRepository();
+        List<Person> list = null;
+        public ViewResult Index()
+        {
+            list = person.GetUsers();
+
+            return View(list);
+        }
+    }
+}
---
+++ WebApplication1/Models/PersonRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace WebApplication1.Models
+{
+    public class PersonRepository
+    {
+        const string conStr = "Data Source=localhost; Initial Catalog=alif;Integrated Security=True";
+
+        public List<Person> GetUsers()
+        {
+            List<Person> persons = new List<Person>();
+            using (IDbConnection db = new SqlConnection(conStr))
+            {
+                persons = db.Query<Person>("SELECT * FROM Person").ToList();
+            }
+            return persons;
+        }
+    }
+}
